d990971dfab049ec8e2f7816ffb67aabbb9d604b
Nick Bergquist <nick_bergquist@yahoo.co.uk>
2020-05-22 10:43:58 +0100

    Add viewprt handling

--- out/main.js
+++ out/main.js
@@ -5,9 +5,38 @@
         init: function() {
 
             var $_ = this;
+            var eleBody = document.querySelector('body');
 
-            $_.initFooter();
+            $_.updateDOM();
             $_.bindEvents();
+
+            // viewport handling - mobile nav
+            if(window.innerWidth >= 480) {
+
+                eleBody.classList.add('view-large');
+                $_.siteNavBehaviour.navMaximised();
+            }
+            else {
+
+                eleBody.classList.add('view-small');
+                $_.siteNavBehaviour.navMinimised();
+            }
+
+            window.onresize = function() {
+
+                if(window.innerWidth >= 480) {
+
+                    eleBody.classList.remove('view-small');
+                    eleBody.classList.add('view-large');
+                    $_.siteNavBehaviour.navMaximised();
+                }
+                else {
+
+                    eleBody.classList.remove('view-large');
+                    eleBody.classList.add('view-small');
+                    $_.siteNavBehaviour.navMinimised();
+                }
+            }
         },
 
         bindEvents: function() {
@@ -24,13 +53,38 @@
             });
         },
 
-        initFooter: function() {
+        updateDOM: function() {
 
             var eleYear = document.querySelector('#year');
             var dateToday = new Date();
             var year = dateToday.getFullYear();
 
             eleYear.textContent = year;
+        },
+
+        siteNavBehaviour: {
+
+            navMinimised: function() {
+
+                var btnExistingMenuControl = document.querySelector('header .nav-button-show');
+
+                if(!btnExistingMenuControl) {
+
+                    var headerNav = document.querySelector('#siteNav');
+                    var btnNav = '<button type="button" class="nav-button-show" aria-expanded="false" aria-controls="siteNav"><span>Show/Hide</span></button>';
+            
+                    // ToDo: 
+                    // Add the button to the DOM.
+                    // Add click events
+                    // Add keyboard navigation
+                }
+            },
+
+            navMaximised: function() {
+
+                // ToDo:
+                // Reset
+            }
         }
     }
 
